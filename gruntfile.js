6daea18b030d0bb729811ea8edee5f30b6104ca8
WilfredoSolorzano <wsolorzano00@gmail.com>
2024-03-27 20:43:58 -0300

    nueva verção

--- gruntfile.js
+++ gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt){
                         },
                         {
                             match: 'ENDERECO_DE_JS',
-                            replacement: './src/scripts/main.min.js'
+                            replacement: './scripts/main.min.js'
                         }
                     ]
                 },
